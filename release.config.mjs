11e7d13f9a126139c7b8c4356a7df2cf258e62ec
grant <ruddickmg@gmail.com>
2026-02-21 18:49:40 -1000

    set up release process for binaries

---
+++ release.config.mjs
@@ -0,0 +1,21 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  branches: ["main", "set-up-ci"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/github",
+      {
+        "assets": [
+          { "path": "target/release/libhermes.dylib", "label": "MacOS" },
+          { "path": "target/release/libhermes.so", "label": "Linux" },
+          // TODO: Figure out how
+          // { "path": "target/release/hermes.lld", "label": "Windows" },
+        ]
+      }
+    ]
+  ]
+}
