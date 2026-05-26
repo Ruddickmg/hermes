cb55489d74e1c97bace27eb152f9071bb9fbef32
grant <ruddickmg@gmail.com>
2026-05-25 20:13:13 -1000

    make git hooks work out of box

--- build.rs
+++ build.rs
@@ -1,3 +1,11 @@
 fn main() -> Result<(), nvim_oxi::tests::BuildError> {
+    let src = std::path::Path::new(".githooks/pre-commit");
+    let dst = std::path::Path::new(".git/hooks/pre-commit");
+    if src.exists() {
+        std::fs::copy(src, dst).ok();
+        #[cfg(unix)]
+        std::fs::set_permissions(dst, std::os::unix::fs::PermissionsExt::from_mode(0o755)).ok();
+    }
+
     nvim_oxi::tests::build()
 }
