c6e85c7c02d05c1a8531269ccef68f4d2bb878e3
Marcus Ruddick <Ruddickmg@gmail.com>
2026-07-27 18:39:19 -1000

    fix: split commit from release (#172)
    * fix: split release from hash commit
    
    * use semantic release for commits back to repo

--- release.config.mjs
+++ release.config.mjs
@@ -9,6 +9,11 @@ export default {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    ["@semantic-release/npm", { npmPublish: false }],
+    ["@semantic-release/git", {
+      assets: ["nix/sources.json", "package.json"],
+      message: "chore(release): ${nextRelease.version} [skip ci]"
+    }],
     [
       "@semantic-release/github",
       {
